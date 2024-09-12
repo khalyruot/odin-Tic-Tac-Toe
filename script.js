ac577b78f1a071faaa86ac696a7109dfd2be7d97
khaly <khalyruot12345@gmail.com>
2024-09-12 16:04:50 +0700

    add 3 row, and 3 column

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-function showMessage() {
-    document.getElementById("message").innerText = "Hello! You clicked the button!";
+const playerOneName = "Jame";
+const playerOneMarker = "X";
+const playerTwoName = "David";
+const playerTwoMarer = "O";
+
+const playerOne = {
+    name: "Jame", 
+    marker: "X"
+};
+
+const playerTwo = {
+    name: "David",
+    marker: "O"
+};
+
+
+
+var container = document.querySelector('.grid_area');
+
+function tictactoeGame(playerOne, playerTwo){
+    
+    for(let i = 0; i<3; i++){
+        const box_3x3_row = document.createElement('div');
+        box_3x3_row.classList.add('content');
+        container.appendChild(box_3x3_row);
+        
+        for(let j = 0; j < 3; j++){
+            const box_3x3_column = document.createElement('div');
+            box_3x3_column.textContent = "";
+            box_3x3_row.appendChild(box_3x3_column);
+            box_3x3_column.classList.add('sub_content');
+            
+        }
+    }
+
+    return "a winner";
+
 }
+
+tictactoeGame();
