d7e7aa68756d51b61038597c59698938437d325e
khaly <khalyruot12345@gmail.com>
2024-09-14 10:23:40 +0700

    add if, else statement

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const playerTwo = {
     marker: "O"
 };
 
+const choicePlayer = {}
 
 
 var container = document.querySelector('.grid_area');
@@ -29,22 +30,120 @@ function tictactoeGame(playerOne, playerTwo){
             box_3x3_column.textContent = "";
             box_3x3_row.appendChild(box_3x3_column);
             box_3x3_column.classList.add('sub_content');
+            console.log("j:  " + j);
+            console.log("i: " + i);
+
             
         }
-
+    }
+    
+    for(let k=0; k<3; k++){
         const box_hover = document.querySelectorAll('.sub_content');
+        console.log("k: " + k);
     
-        for(let k = 0; k < box_hover.length; k++){
+        for(let l = 0; l < box_hover.length; l++){
+
+            const player = 5;
     
-            box_hover[k].onclick = function(){mouseOnClick()};
+            box_hover[l].onclick = function(){mouseOnClick()};
             function mouseOnClick(){
-                box_hover[k].style.backgroundColor ="red";
+                if(player%2 == 0){
+                    const para = document.createElement("p");
+                    para.innerHTML = "X";
+                    box_hover[l].appendChild(para);
+                    console.log("l: " + l);
+                    boardGame_push(l);
+                   
+                }
+                else{
+                    const para = document.createElement("p");
+                    para.innerHTML = "O";
+                    box_hover[l].appendChild(para);
+                    console.log("l: " + l);
+                    boardGame_push(l);
+                    console.log(choicePlayer);
+                }
             }
         } 
+
     }
+
+       
+    
 
     return "a winner";
 
+}
+
+
+
+function boardGame_push(l){
+    const keys_boardGame = [];
+    const values_boardGame = [];
+    if(l==0){
+        keys_boardGame.push("a");
+        values_boardGame.push(0);
+        keys_boardGame.forEach((key, index) => {
+            choicePlayer[key] = values_boardGame[index];
+        });
+    }
+    else if(l==1){
+        keys_boardGame.push("b");
+        values_boardGame.push(0);
+        keys_boardGame.forEach((key, index) => {
+            choicePlayer[key] = values_boardGame[index];
+        });
+    }
+    else if(l==2){
+        keys_boardGame.push("c");
+        values_boardGame.push(0);
+        keys_boardGame.forEach((key, index) => {
+            choicePlayer[key] = values_boardGame[index];
+        });
+    }
+    else if(l==3){
+        keys_boardGame.push("a");
+        values_boardGame.push(1);
+        keys_boardGame.forEach((key, index) => {
+            choicePlayer[key] = values_boardGame[index];
+        });
+    }
+    else if(l==4){
+        keys_boardGame.push("b");
+        values_boardGame.push(1);
+        keys_boardGame.forEach((key, index) => {
+            choicePlayer[key] = values_boardGame[index];
+        });
+    }
+    else if(l==5){
+        keys_boardGame.push("c");
+        values_boardGame.push(1);
+        keys_boardGame.forEach((key, index) => {
+            choicePlayer[key] = values_boardGame[index];
+        });
+    }
+    else if(l==6){
+        keys_boardGame.push("a");
+        values_boardGame.push(2);
+        keys_boardGame.forEach((key, index) => {
+            choicePlayer[key] = values_boardGame[index];
+        });
+    }
+    else if(l==7){
+        keys_boardGame.push("b");
+        values_boardGame.push(2);
+        keys_boardGame.forEach((key, index) => {
+        choicePlayer[key] = values_boardGame[index];
+        });
+    }
+    else if(l==8){
+        keys_boardGame.push("c");
+        values_boardGame.push(2);
+        keys_boardGame.forEach((key, index) => {
+            choicePlayer[key] = values_boardGame[index];
+        });
+    }
+    return choicePlayer;
 }
 
 function playGame(){
@@ -73,10 +172,6 @@ function playGame(){
         }
     }
 
-    console.log(choicePlayerRow);
-    console.log(choicePlayerNumber);
-    console.log(choicePlayerNumber[1]===choicePlayerNumber[2]);
-
 
     if(choicePlayerRow[0]!==choicePlayerRow[1] && choicePlayerRow[1]!==choicePlayerRow[2] && choicePlayerRow[0]!==choicePlayerRow[2] &&  choicePlayerNumber[0]!==choicePlayerNumber[1] && choicePlayerNumber[1]!==choicePlayerNumber[2] && choicePlayerNumber[0]!==choicePlayerNumber[2]){
         console.log("You're the Winner!!");
@@ -90,12 +185,8 @@ function playGame(){
     else{
         console.log("You're the Loser!!")
     }
-
-   
-
-   
-
 }
 
 tictactoeGame();
 playGame();
+boardGame_push();
